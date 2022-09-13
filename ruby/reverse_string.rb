a07419fa58ca979e2c094ee3a5c646d0c530093b
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-09-13 15:06:15 -0400

    ruby

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,13 @@
 def reverse_string(str)
   # type your code in here
+  reversed = ""
+
+  str.chars.each do |eachChar|
+    reversed = eachChar += reversed 
+  end
+  
+  return reversed
+
 end
 
 if __FILE__ == $PROGRAM_NAME
